4646c6bf817680cd3161803329d6533063e3bf00
TiagoPradoMartins <tiagopradomartins@gmail.com>
2019-09-22 22:18:47 -0300

    Primeira versao

--- SQLite/MainActivity.java
+++ SQLite/MainActivity.java
@@ -13,31 +13,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Cria banco de dados
-        SQLiteDatabase bancoDados = openOrCreateDatabase("app" , MODE_PRIVATE , null);
+        try {
+            //Cria banco de dados
+            SQLiteDatabase bancoDados = openOrCreateDatabase("app", MODE_PRIVATE, null);
 
-        //Cria tabela no banco
-        bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (nome VARCHAR , idade INT(3))");
+            //Cria tabela no banco
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (nome VARCHAR , idade INT(3))");
 
-        //Inserir dados
-        bancoDados.execSQL("INSERT INTO pessoas (nome , idade) VALUES ('Tiago' , 32)");
-        bancoDados.execSQL("INSERT INTO pessoas (nome , idade) VALUES ('Danuza' , 64)");
+            //Inserir dados
+            bancoDados.execSQL("INSERT INTO pessoas (nome , idade) VALUES ('Tiago' , 32)");
+            bancoDados.execSQL("INSERT INTO pessoas (nome , idade) VALUES ('Danuza' , 64)");
 
-        //Resgatar dados do banco
-        Cursor cursor = bancoDados.rawQuery("SELECT nome , idade FROM pessoas",null);
+            //Resgatar dados do banco
+            Cursor cursor = bancoDados.rawQuery("SELECT nome , idade FROM pessoas", null);
 
             //Recupera o indice
             int indiceCelulaNome = cursor.getColumnIndex("nome");
             int indiceCelulaIdade = cursor.getColumnIndex("idade");
 
-        //Retornar o cursor para o inicio da tabela
-        cursor.moveToFirst();
+            //Retornar o cursor para o inicio da tabela
+            cursor.moveToFirst();
 
-        //Apresentando o resultado
-        while (cursor != null){
-            Log.i("RESULTADO - Nome  " , cursor.getString(indiceCelulaNome));
-            Log.i("RESULTADO - Idade " , cursor.getString(indiceCelulaIdade));
-            cursor.moveToNext();
+            //Apresentando o resultado
+            while (cursor != null) {
+                Log.i("RESULTADO - Nome  ", cursor.getString(indiceCelulaNome));
+                Log.i("RESULTADO - Idade ", cursor.getString(indiceCelulaIdade));
+                cursor.moveToNext();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
         }
 
     }
